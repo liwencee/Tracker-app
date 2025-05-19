151105cdc7fe43fedd5a03ce361ae6db64a48c10
Olalekan Akindiya <lehqandiya@gmail.com>
2025-05-19 22:02:55 +0100

    Add frontend and backend folders

--- deploy.sh
+++ deploy.sh
@@ -1,30 +1,15 @@
-      - name: Deploy to EC2
-        env:
-          AWS_REGION: ${{ secrets.AWS_REGION }}
-          ECR_URI: ${{ env.ECR_URI }}
-        run: |
-          # Set target EC2 based on branch
-          if [[ "${GITHUB_REF##*/}" == "main" ]]; then
-            TARGET_USER=${{ secrets.EC2_USER_PROD }}
-            TARGET_HOST=${{ secrets.EC2_HOST_PROD }}
-          else
-            TARGET_USER=${{ secrets.EC2_USER_STAGING }}
-            TARGET_HOST=${{ secrets.EC2_HOST_STAGING }}
-          fi
+#!/bin/bash
 
-          mkdir -p ~/.ssh
-          echo "${{ secrets.SSH_PRIVATE_KEY }}" > ~/.ssh/id_rsa
-          chmod 600 ~/.ssh/id_rsa
-          ssh-keyscan -H "$TARGET_HOST" >> ~/.ssh/known_hosts
+set -e
 
-          # Upload deployment files
-          scp -i ~/.ssh/id_rsa docker-compose.prod.yml deploy.sh $TARGET_USER@$TARGET_HOST:/home/$TARGET_USER/
-          scp -r -i ~/.ssh/id_rsa frontend backend $TARGET_USER@$TARGET_HOST:/home/$TARGET_USER/
+# Set environment
+BRANCH=$(basename $(git symbolic-ref HEAD 2>/dev/null || echo "refs/heads/main"))
+export BRANCH=$BRANCH
 
-          # Run deploy.sh remotely
-          ssh -i ~/.ssh/id_rsa $TARGET_USER@$TARGET_HOST << EOF
-            chmod +x /home/$TARGET_USER/deploy.sh
-            aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_URI
-            ./deploy.sh
-          
-          EOF
+# Pull latest images
+docker pull $ECR_URI/tasktracker-frontend:$BRANCH
+docker pull $ECR_URI/tasktracker-backend:$BRANCH
+
+# Deploy
+docker-compose -f docker-compose.prod.yml down
+docker-compose -f docker-compose.prod.yml up -d
