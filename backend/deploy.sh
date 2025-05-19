7efa63ef64fdfb9ea31a9d88934da3b1135c08c6
Olalekan Akindiya <lehqandiya@gmail.com>
2025-05-19 20:38:09 +0100

    added backend

--- backend/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd /home/ubuntu
-
-echo "[INFO] Stopping previous containers..."
-docker-compose -f docker-compose.prod.yml down
-
-echo "[INFO] Pulling latest code and rebuilding..."
-docker-compose -f docker-compose.prod.yml up -d --build
